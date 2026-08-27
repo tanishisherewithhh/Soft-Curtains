7966d975604ca7121f9e953f9eac71a6eecf1f71
tanishisherewith <120117618+tanishisherewithhh@users.noreply.github.com>
2026-08-27 14:03:35 +0530

    set modid

--- src/main/java/com/tanishisherewith/SoftCurtainsMain.java
+++ src/main/java/com/tanishisherewith/SoftCurtainsMain.java
@@ -1,4 +1,4 @@
-package com.example;
+package com.tanishisherewith;
 
 import net.fabricmc.api.ModInitializer;
 
@@ -7,12 +7,9 @@ import net.minecraft.resources.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
-	public static final String MOD_ID = "modid";
+public class SoftCurtainsMain implements ModInitializer {
+	public static final String MOD_ID = "softcurtains";
 
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
